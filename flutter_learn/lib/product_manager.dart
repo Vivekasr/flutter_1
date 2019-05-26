1fafba3fd8044c29c7934592418a871cc47919d6
Vivekasr <hie.vivi.sharma@gmail.com>
2019-05-26 14:36:27 +0530

    semi_build3
    changed theme color scheme

--- flutter_learn/lib/product_manager.dart
+++ flutter_learn/lib/product_manager.dart
@@ -3,20 +3,42 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct){
+    print('[ProductManager Widget] Constructor');
+  }
+
   @override
   State<StatefulWidget> createState() {
+    print('[ProductManager Widget] createState()');
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    print('[ProductManager Widget] initState()');
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[ProductsManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
  
   @override
   Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
     return Column(children: [Container(
       margin: EdgeInsets.all(10.0),
       child: RaisedButton(
+        color: Theme.of(context).primaryColor,
         onPressed: () {
           setState(() {
             _products.add('Advanced Food Tester');
